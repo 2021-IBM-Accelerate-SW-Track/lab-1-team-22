3815cdf1ec371d63cf620da5975bfb64b5145e98
Ashley Slater <orecodev@gmail.com>
2021-06-15 12:59:27 -0700

    Added materialUI pkg

--- src/App.js
+++ src/App.js
@@ -3,8 +3,30 @@ import tasks from './tasks.json'
 import TodoList from './ToDoList'
 import TodoForm from './ToDoForm'
 import "./App.css";
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+ 
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
  
 function App() {
+
+  const classes = useStyles()
   const [ todoList, setTodoList ] = useState(tasks)
 
   const handleToggle = (id) => {
@@ -28,10 +50,18 @@ function App() {
   }
 
  return (
-   <div className="App">
-       <TodoList todoList={todoList} handleToggle={handleToggle} handleFilter={handleFilter}/>
-      <TodoForm addTask={addTask}/>
-   </div>
+   <Card className={classes.root} variant="outlined">
+    <CardContent>
+    <Typography className={classes.title} color="textSecondary" gutterBottom>
+          Word of the Day       <TodoList todoList={todoList} handleToggle={handleToggle} handleFilter={handleFilter}/>
+        </Typography>
+        <CardActions>
+        <Button size="small">   
+        <TodoForm addTask={addTask}/></Button>
+      </CardActions>
+    
+   </CardContent>
+   </Card>
  );
 }
  
--- src/ToDoForm.js
+++ src/ToDoForm.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 //import { useForm } from 'react-hook-form'
 
 
+
 const TodoForm = ({ addTask }) => {
   const [userTask, setUserTask] = useState("");
 
@@ -14,18 +15,18 @@ const TodoForm = ({ addTask }) => {
     addTask(userTask);
     setUserTask("");
   };
+
   return (
     <form onSubmit={handleSubmit}>
-      <input 
+      <input
       data-testid="new-item-input"
         onChange={handleChange}
         type="text"
         name="todo"
-        id="new-item-input"
         value={userTask}
         placeholder="Add a new Task"
       />
-      <button id="new-item-button" data-testid="new-item-button"> Submit</button>
+      <button data-testid="new-item-button"> Submit</button>
     </form>
   );
 };
