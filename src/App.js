197eb54595a23d2b4f178eb46933d8634db53a7f
SamuelMuvdi01 <78106875+SamuelMuvdi01@users.noreply.github.com>
2021-06-17 17:57:14 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,86 +1,97 @@
 import React, { Component } from 'react';
 
-class App extends Component{
-  constructor(props){
-    super(props);
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      newItem: "",
-      list:[]
-    }
-  }
-
-  updateInput(key, value){
-    this.setState({
-      [key]: value
-    });
-  }
-
-  addItem(){
-    const newItem={
-      id: 1+ Math.random(),
-      value: this.state.newItem.slice()
-    };
-    const list = [...this.state.list];
-
-    list.push(newItem);
-
-    this.setState({
-    list,
-    newItem:""      
-    });
+        this.state = {
+            newItem: "",
+            list: []
+        }
     }
 
-    deleteItem(id){   //add console log//
-      const list = [...this.state.list];
-
-      const updatedList = list.filter(item => item.id !== id);
-
-      this.setState({list: updatedList});
+    updateInput(key, value) {
+        this.setState({
+            [key]: value
+        });
     }
-  
-  render(){
-    return(
-      <div className = "App">
-        <div>
-          Add an item...
-          <br/>
-          <input
-          type = "text"
-          placeholder = "Type item here..."
-          value = {this.state.newItem}
-          onChange={e => this.updateInput("newItem", e.target.value)}
-        />
-        <button
-        onClick={() => this.addItem()}
-        >
-          Add
-          </button>
 
-          <br/>
+    addItem() {
+        const newItem = {
+            id: 1 + Math.random(),
+            value: this.state.newItem.slice()
+        };
+        const list = [...this.state.list];
 
-<ul>
-  {this.state.list.map(item=>{
-    return(
-      <li key={item.id}>
-        {item.value}
-        <button
-        onClick={() => this.deleteItem(item.id)}
+        list.push(newItem);
 
-        >
-          X
+        console.log('New item added');
 
-        </button>
-        </li>
-    )
-  })
-  }
-  </ul> 
-         </div>
+        this.setState({
+            list,
+            newItem: ""
+        });
+    }
 
-      </div>
-    );
-  }
-  }
+    deleteItem(id) { 
+        const list = [...this.state.list];
+
+        const updatedList = list.filter(item => item.id !== id);
+
+        this.setState({ list: updatedList });
+        console.log('Item deleted');
+    }
+
+
+    render() {
+        const myStyle = {
+            color: "black",
+            backgroundColor: "lightblue",
+            padding: "10px",
+            fontFamily: "Arial",
+            fontSize: "36px",
+            textAlign: "center"
+        };
+
+        const myStyle2 = {
+            color: "black",
+            backgroundColor: "white",
+            padding: "10px",
+            fontFamily: "Arial",
+            fontSize: "36px",
+            textAlign: "center"
+        };
+        return (<
+            div className = "App" >
+            <div style = {myStyle}> Add an item... <br/>
+            <
+            input style = {myStyle2} type = "text"
+            placeholder = "Type item here..."
+            value = { this.state.newItem }
+            onChange = { e => this.updateInput("newItem", e.target.value) }/> 
+
+            <button onClick={()=>this.addItem()}> Add    </button>
+
+
+            <ul style = {{textAlign: "center"}}> {
+                this.state.list.map(item => {
+                    return ( <
+                        li key = { item.id } > { item.value } < button onClick = {()=>this.deleteItem(item.id)}> X </button> 
+                        </li >
+                        )
+                    })
+            } 
+            </ul> 
+
+            <body style = {myStyle}></body>
+             </div>
+
+            
+            
+           
+            </div>
+        );
+    }
+}
 
 export default App;
